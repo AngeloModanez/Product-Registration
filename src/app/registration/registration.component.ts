066fdf264a3813b5e9c37b10a9c6f06d8442a5d4
Bananadev3 <angelomodanez30@gmail.com>
2024-03-23 20:09:29 -0300

    Reactive form, product interface, product Array and save button.

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,10 +1,27 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Product } from './../Product';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.css'
+  styleUrl: './registration.component.css',
 })
 export class RegistrationComponent {
+  product: Product[] = [];
+  productFormGroup: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.productFormGroup = formBuilder.group({
+      id: [''],
+      name: [''],
+      price: [''],
+      quantity: [''],
+      description: [''],
+    });
+  }
+
+  save() {
+    this.product.push(this.productFormGroup.value);
+  }
 }
